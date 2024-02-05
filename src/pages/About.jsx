29341198217b0ac376f9002acc638e6d60b0dda1
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-02-05 13:53:44 +0000

    add Landing, About and Home to the pages folder

---
+++ src/pages/About.jsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <p>I am the about page!</p>;
+};
+
+export default About;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,35 @@
+import NcLogo from "../assets/nc-logo.svg";
+import { Link } from "react-router-dom";
+import Users from "../components/Users";
+
+const Landing = () => {
+  return (
+    <>
+      <header>
+        <Link to="/about">
+          <img
+            src={NcLogo}
+            className="logo northcoders"
+            alt="Northcoders logo"
+          />
+        </Link>
+        <h1>Northcoders News</h1>
+      </header>
+      <section className="users">
+        <Users />
+
+        {/* <Link to="/home">
+          <button>View Articles</button>
+        </Link> */}
+        {/* <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p> */}
+      </section>
+      <p className="more-information">
+        Click on the Northcoders logo to learn more about this project
+      </p>
+    </>
+  );
+};
+
+export default Landing;
