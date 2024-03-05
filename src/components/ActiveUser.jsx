176d0dfb69b6b19c399875c7d1f7943901799847
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-03-05 11:25:06 +0000

    remove unused imports

--- src/components/ActiveUser.jsx
+++ src/components/ActiveUser.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import UserContext from "../contexts/UserContext";
 import { Link } from "react-router-dom";
 
