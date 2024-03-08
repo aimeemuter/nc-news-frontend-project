2446b1403ebc189840ff9f95c0a6f5856d8b454a
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-03-08 13:41:31 +0000

    add content to the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,21 @@
 const About = () => {
-  return <p>I am the about page!</p>;
+  return (
+    <>
+      <p>
+        This is my Northcoders News frontend project. I created this website
+        using JavaScript, React and React Router.
+        <p>
+          All users, articles and comments are fetched from my backend
+          PostgreSQL API.
+        </p>
+      </p>
+      <p>
+        Feel free to navigate through the website and leave likes, dislikes and
+        comments. Thank you for checking out my project, I hope you have a
+        lovely day!
+      </p>
+    </>
+  );
 };
 
 export default About;
